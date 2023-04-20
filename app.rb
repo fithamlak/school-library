96b242df6fb76387c1fc96331527233caa8fb384
fithamlak <justiceofgod01@gmail.com>
2023-04-20 18:40:42 +0300

    Implement the method to List all books

--- app.rb
+++ app.rb
@@ -3,8 +3,20 @@ require './teacher'
 require './rental'
 require './book'
 
-class App
+class Ap
+  attr_reader :books
 
   def initialize
+    @books = []
+  end
+
+  def list_books
+    if @books == []
+      puts 'oops, the store is empty now :-('
+    else
+      @books.each_with_index do |book, index|
+        puts %(#{index + 1} - Title: "#{book.title}", Author: #{book.author})
+      end
+    end
   end
 end
