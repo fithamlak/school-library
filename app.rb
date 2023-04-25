107bc9b5375e1c786c31b19f8db32904197236c8
fithamlak <justiceofgod01@gmail.com>
2023-04-25 15:44:24 +0300

    add Refactor of rentals related functionalities from app.rb

--- app.rb
+++ app.rb
@@ -4,24 +4,18 @@ require './rental'
 require './book'
 require './books_list'
 require './people_list'
+require './rentals_list'
 
 class App
   attr_reader :books, :people
 
   def initialize
-    @books_list BooksList.new
+    @books_list = BooksList.new
     @books = @books_list.books
     @people_list = PeopleList.new
     @people = @people_list.people
-  end
-
-  def add_rental(date, book, person)
-    Rental.new(date, book, person)
-  end
-
-  def rentals(person_id)
-    person = @people.find { |p| p.id == person_id }
-    person.rentals
+    @rentals_list = RentalsList.new
+    @rentals = @rentals_list.rentals
   end
 
   def list_books
@@ -41,50 +35,17 @@ class App
     @people_list.add_person(person)
     puts 'person creatd successsfully!'
   end
-
-  def create_rental
-    return puts "Ops, we don't have any book for rent" if @books == []
-    return puts "Ops, we don't have registered people who can rent books" if @people == []
-
-    puts 'select a book from the following list by number:'
-    @books.each_with_index do |book, index|
-      puts %[#{index}) Title: "#{book.title}", Author: #{book.author}]
-    end
-    selected_book = @books[gets.chomp.to_i]
-    puts %(You selected "#{selected_book.title}" written by #{selected_book.author})
-    puts ''
-
-    puts 'Select a person from the following list by number (not id):'
-    @people.each_with_index do |person, index|
-      puts %[#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}]
-    end
-    selected_person = @people[gets.chomp.to_i]
-    puts "You selected #{selected_person.name}"
-    puts ''
-
-    print 'Date: '
-    entered_date = gets.chomp
-
-    add_rental(entered_date, selected_book, selected_person)
-    puts 'Rental created successfully!'
+  def list_rentals
+    @rentals_list.list_rentals(@people)
+  end
+  def rentals(person_id)
+    person = @people.find { |p| p.id == person_id }
+    person.rentals
   end
 
-  def list_rentals
-    return puts "Sorry, we don't have any registered people" if @people == []
-
-    print 'ID of person: '
-    person_id = gets.chomp.to_i
-    person = @people.find { |p| p.id == person_id }
-    return puts "Sorry, we don't have any person with this ID" unless person
-
-    if person.rentals.any?
-      puts 'Rentals:'
-      person.rentals.each do |rental|
-        puts %(Date: #{rental.date}, Book: "#{rental.book.title}" by #{rental.book.author})
-      end
-    else
-      puts "Ops, we don't have any rentals registered for #{person.name}"
-    end
+  def create_rental
+    rental = Rental.create_rental(@books, @people)
+    @rentals_list.add_rental(rental)
   end
 
   def exit
--- main.rb
+++ main.rb
@@ -1,36 +1,14 @@
 require './app'
-require './options'
 require './menu'
+require './options'
 
 class Main
   def initialize
     puts 'Welcome to School Library App!'
-    @app = App.new
-    @options = Options.new
+     @options = Options.new
+     @app = App.new
   end
 
-  def menu
-    puts 'Please select an option by entering a number:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
-  end
-
-  def options
-    {
-      1 => 'list_books',
-      2 => 'list_people',
-      3 => 'create_person',
-      4 => 'create_book',
-      5 => 'create_rental',
-      6 => 'list_rentals',
-      7 => 'exit'
-    }
-  end
 
   def run
     Menu.new
@@ -39,7 +17,7 @@ class Main
     puts "You choose option #{selected}"
     case selected
     when 1..7
-      @app.send(options.get_option(selected))
+      @app.send(@options.get_option(selected))
     else
       puts 'Please choose a valid option:'
     end
