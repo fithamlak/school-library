e80dc8e79be131e4b015c7c5255ae4103b53aea7
fithamlak <justiceofgod01@gmail.com>
2023-04-25 14:59:20 +0300

    add Refactor of people of person(teacher and student) related functionalities from app.rb

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require './teacher'
 require './rental'
 require './book'
 require './books_list'
+require './people_list'
 
 class App
   attr_reader :books, :people
@@ -10,15 +11,8 @@ class App
   def initialize
     @books_list BooksList.new
     @books = @books_list.books
-    @people = []
-  end
-
-  def add_teacher(teacher)
-    @people.push(teacher) unless @people.include?(teacher)
-  end
-
-  def add_student(student)
-    @people.push(student) unless @people.include?(student)
+    @people_list = PeopleList.new
+    @people = @people_list.people
   end
 
   def add_rental(date, book, person)
@@ -39,80 +33,13 @@ class App
   end
 
   def list_people
-    if @people == []
-      puts 'no lits of people yet here :-('
-    else
-      @people.each_with_index do |person, index|
-        puts %(#{index + 1} - [#{person.class}] Name: #{person.name}, ID: 3{person.id}, Age: #{person.age})
-      end
-    end
+    @people_list.list_people
   end
 
   def create_person
-    print 'what do you want to create a student (1) or a teacher (2)? [Input the your choice number]: '
-    option = gets.chomp.to_i
-    case option
-    when 1
-      create_student
-    when 2
-      create_teacher
-    else
-      puts 'please select a valid option:'
-      create_person
-    end
-  end
-
-  def create_student
-    age = gets_age
-    name = gets_name
-    classroom = gets_classroom
-    parent_permission = gets_parent_permission
-    student = Student.new(age, classroom, name, parent_permission: parent_permission)
-    add_student(student)
+    person = Person.create_person
+    @people_list.add_person(person)
     puts 'person creatd successsfully!'
-  end
-
-  def create_teacher
-    age = gets_age
-    name = gets_name
-    specialization = gets_specialization
-    teacher = Teacher.new(age, specialization, name)
-    add_teacher(teacher)
-    puts 'person created successfully!'
-  end
-
-  def gets_age
-    print 'Age: '
-    gets.chomp
-  end
-
-  def gets_classroom
-    print 'Classroom: '
-    gets.chomp
-  end
-
-  def gets_name
-    print 'Name: '
-    gets.chomp
-  end
-
-  def gets_parent_permission
-    print 'Does he or she has parent permission? [Y/N]: '
-    parent_permission = gets.chomp.upcase
-    case parent_permission
-    when 'Y'
-      true
-    when 'N'
-      false
-    else
-      puts 'please enter a valid option'
-      gets_parent_persmission
-    end
-  end
-
-  def gets_specialization
-    print 'specialization'
-    gets.chomp
   end
 
   def create_rental
