c5f612cdb8f6833dcecf8c485107372a38d24c8d
fithamlak <justiceofgod01@gmail.com>
2023-04-28 16:13:17 +0300

    linter correction for nameable test

--- spec/nameable_test_spec.rb
+++ spec/nameable_test_spec.rb
@@ -3,7 +3,6 @@ require './nameable'
 describe Nameable do
   it 'expect to show NotImplementedError' do
     n = Nameable.new
-    expect { n.correct_name }.to
-    raise_error(NotImplementedError)
+    expect { n.correct_name }.to raise_error(NotImplementedError)
   end
 end
