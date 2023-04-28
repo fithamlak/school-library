06ec7831eeba3615d9cdbb44db93cc9ac551d400
fithamlak <justiceofgod01@gmail.com>
2023-04-28 15:49:37 +0300

    linters correction

--- spec/nameable_test_spec.rb
+++ spec/nameable_test_spec.rb
@@ -1,9 +1,9 @@
-require './nameable' 
+require './nameable'
 
-describe Nameable do 
-  it 'expect to show NotImplementedError' do 
-    n = Nameable.new 
-    expect { n.correct_name }.to 
-    raise_error(NotImplementedError) 
-  end 
-end 
+describe Nameable do
+  it 'expect to show NotImplementedError' do
+    n = Nameable.new
+    expect { n.correct_name }.to
+    raise_error(NotImplementedError)
+  end
+end
